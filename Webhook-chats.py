8773e607143b9e178104c2abd6a4d9a4fbc2e790
iRio <68218339+iRio7@users.noreply.github.com>
2021-06-06 16:33:02 +0800

    Add files via upload

---
+++ Webhook-chats.py
@@ -0,0 +1,19 @@
+# Chat via webhooks
+# iRio#0007
+
+# Modules
+import requests # > pip install requests
+import os
+import time
+from time import sleep
+from datetime import datetime # > pip install datetime
+
+time = datetime.now()
+clock = time.strftime(' %H:%M %p')
+os.system('title Webhook - Chat')
+
+os.system('color d')
+chats = input('Messages :')
+message = {"content": '**Chat** : ' + chats + '\nAt Time : ' + clock}
+print(f"[INFO] : Message sended")
+response = requests.post("your webhook links", json=message)
